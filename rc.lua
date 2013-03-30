b1fa66072d9edd906e942f329805bfabf79bb542
Dumitru Ursu <dima@ceata.org>
2013-03-30 23:44:18 +0200

    took out my autorun programs

--- rc.lua
+++ rc.lua
@@ -27,37 +27,11 @@ require("menu")
 -- w - means widget
 require("w_keyboard")
 -- c - means custom
+-- I use c prefix whenever I think my modules name can conflic with 
+-- an existing lua library. It's not the best way to deal with namespace 
+-- conflicts, but it works
 require("c_wibox")
 require("rules")
 require("c_signals")
-
---{{ autorun stuff :D
-function run_once(prg)
-  awful.util.spawn_with_shell("pgrep -u $USER -x " .. prg .. " || (" .. prg .. ")")
-end
-
-run_once("nohup /usr/lib/gnome-settings-daemon/gnome-settings-daemon")
-run_once("conky") 
-run_once("conky -c ~/.conkyrc.task") 
-run_once("xautolock")
-run_once("claws-mail")
---run_once("luakit")
---run_once("wicd-client")
---run_once("owncloud")
---run_once("ssh-agent")
-run_once("audacious")
---run_once("anki")
---run_once("transmission-gtk")
---run_once("sparkleshare start")
---run_once("xcompmgr")
-run_once("nm-applet")
-run_once("redshift")
---run_once("liferea")
-run_once("xchat")
-run_once("pidgin")
-run_once("shutter")
-run_once("xrdb ~/.Xresources")
-
---}}
-
+require("autorun")
 
